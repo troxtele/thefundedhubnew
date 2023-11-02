bccde0b4ce5b98c906294732d77a2af1f6ba864a
troxtele <105340164+troxtele@users.noreply.github.com>
2023-11-03 03:19:38 +0600

    Add files via upload

---
+++ assets/react-loader-spinner-2be84f58.js
@@ -0,0 +1,161 @@
+import{r as un,a as _}from"./react-eaab5ef2.js";import{W as l,s as o}from"./styled-components-edc81f87.js";import{p as hn}from"./styled-tools-bf41095e.js";var cn="#4fa94d",fn={"aria-busy":!0,role:"status"},m=globalThis&&globalThis.__assign||function(){return m=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},m.apply(this,arguments)},y=globalThis&&globalThis.__assign||function(){return y=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},y.apply(this,arguments)},v=globalThis&&globalThis.__assign||function(){return v=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},v.apply(this,arguments)},O=globalThis&&globalThis.__assign||function(){return O=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},O.apply(this,arguments)},j=globalThis&&globalThis.__assign||function(){return j=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},j.apply(this,arguments)},T=globalThis&&globalThis.__assign||function(){return T=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},T.apply(this,arguments)},d=globalThis&&globalThis.__assign||function(){return d=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},d.apply(this,arguments)},an=globalThis&&globalThis.__makeTemplateObject||function(a,n){return Object.defineProperty?Object.defineProperty(a,"raw",{value:n}):a.raw=n,a},s=242.776657104492,pn=1.6,bn=l(k||(k=an([`
+  12.5% {
+    stroke-dasharray: `,"px, ",`px;
+    stroke-dashoffset: -`,`px;
+  }
+  43.75% {
+    stroke-dasharray: `,"px, ",`px;
+    stroke-dashoffset: -`,`px;
+  }
+  100% {
+    stroke-dasharray: `,"px, ",`px;
+    stroke-dashoffset: -`,`px;
+  }
+`],[`
+  12.5% {
+    stroke-dasharray: `,"px, ",`px;
+    stroke-dashoffset: -`,`px;
+  }
+  43.75% {
+    stroke-dasharray: `,"px, ",`px;
+    stroke-dashoffset: -`,`px;
+  }
+  100% {
+    stroke-dasharray: `,"px, ",`px;
+    stroke-dashoffset: -`,`px;
+  }
+`])),s*.14,s,s*.11,s*.35,s,s*.35,s*.01,s,s*.99);o.path(w||(w=an([`
+  stroke-dasharray: `,"px, ",`;
+  stroke-dashoffset: 0;
+  animation: `," ",`s linear infinite;
+`],[`
+  stroke-dasharray: `,"px, ",`;
+  stroke-dashoffset: 0;
+  animation: `," ",`s linear infinite;
+`])),s*.01,s,bn,pn);var k,w,P=globalThis&&globalThis.__assign||function(){return P=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},P.apply(this,arguments)},$=globalThis&&globalThis.__assign||function(){return $=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},$.apply(this,arguments)},x=globalThis&&globalThis.__assign||function(){return x=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},x.apply(this,arguments)},A=globalThis&&globalThis.__assign||function(){return A=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},A.apply(this,arguments)},E=globalThis&&globalThis.__assign||function(){return E=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},E.apply(this,arguments)},L=globalThis&&globalThis.__assign||function(){return L=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},L.apply(this,arguments)},R=globalThis&&globalThis.__assign||function(){return R=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},R.apply(this,arguments)},g=globalThis&&globalThis.__makeTemplateObject||function(a,n){return Object.defineProperty?Object.defineProperty(a,"raw",{value:n}):a.raw=n,a},i=globalThis&&globalThis.__assign||function(){return i=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},i.apply(this,arguments)},_n=l(W||(W=g([`
+ to {
+    transform: rotate(360deg);
+  }
+`],[`
+ to {
+    transform: rotate(360deg);
+  }
+`]))),mn=[0,30,60,90,120,150,180,210,240,270,300,330],yn=o.svg(C||(C=g([`
+  animation: `,` 0.75s steps(12, end) infinite;
+  animation-duration: `,`s;
+`],[`
+  animation: `,` 0.75s steps(12, end) infinite;
+  animation-duration: `,`s;
+`])),_n,hn("speed","0.75")),vn=o.polyline(D||(D=g([`
+  stroke-width: `,`px;
+  stroke-linecap: round;
+
+  &:nth-child(12n + 0) {
+    stroke-opacity: 0.08;
+  }
+
+  &:nth-child(12n + 1) {
+    stroke-opacity: 0.17;
+  }
+
+  &:nth-child(12n + 2) {
+    stroke-opacity: 0.25;
+  }
+
+  &:nth-child(12n + 3) {
+    stroke-opacity: 0.33;
+  }
+
+  &:nth-child(12n + 4) {
+    stroke-opacity: 0.42;
+  }
+
+  &:nth-child(12n + 5) {
+    stroke-opacity: 0.5;
+  }
+
+  &:nth-child(12n + 6) {
+    stroke-opacity: 0.58;
+  }
+
+  &:nth-child(12n + 7) {
+    stroke-opacity: 0.66;
+  }
+
+  &:nth-child(12n + 8) {
+    stroke-opacity: 0.75;
+  }
+
+  &:nth-child(12n + 9) {
+    stroke-opacity: 0.83;
+  }
+
+  &:nth-child(12n + 11) {
+    stroke-opacity: 0.92;
+  }
+`],[`
+  stroke-width: `,`px;
+  stroke-linecap: round;
+
+  &:nth-child(12n + 0) {
+    stroke-opacity: 0.08;
+  }
+
+  &:nth-child(12n + 1) {
+    stroke-opacity: 0.17;
+  }
+
+  &:nth-child(12n + 2) {
+    stroke-opacity: 0.25;
+  }
+
+  &:nth-child(12n + 3) {
+    stroke-opacity: 0.33;
+  }
+
+  &:nth-child(12n + 4) {
+    stroke-opacity: 0.42;
+  }
+
+  &:nth-child(12n + 5) {
+    stroke-opacity: 0.5;
+  }
+
+  &:nth-child(12n + 6) {
+    stroke-opacity: 0.58;
+  }
+
+  &:nth-child(12n + 7) {
+    stroke-opacity: 0.66;
+  }
+
+  &:nth-child(12n + 8) {
+    stroke-opacity: 0.75;
+  }
+
+  &:nth-child(12n + 9) {
+    stroke-opacity: 0.83;
+  }
+
+  &:nth-child(12n + 11) {
+    stroke-opacity: 0.92;
+  }
+`])),function(a){return a.width});function kn(a){var n=a.strokeColor,r=n===void 0?cn:n,e=a.strokeWidth,t=e===void 0?"5":e,h=a.animationDuration,en=h===void 0?"0.75":h,c=a.width,sn=c===void 0?"96":c,f=a.visible,on=f===void 0?!0:f,p=a.ariaLabel,ln=p===void 0?"rotating-lines-loading":p,gn=un.useCallback(function(){return mn.map(function(b){return _.createElement(vn,{key:b,points:"24,12 24,4",width:t,transform:"rotate(".concat(b,", 24, 24)")})})},[t]);return on?_.createElement(yn,i({xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 48 48",width:sn,stroke:r,speed:en,"data-testid":"rotating-lines-svg","aria-label":ln},fn),gn()):null}var W,C,D,I=globalThis&&globalThis.__assign||function(){return I=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},I.apply(this,arguments)},U=globalThis&&globalThis.__assign||function(){return U=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},U.apply(this,arguments)},z=globalThis&&globalThis.__assign||function(){return z=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},z.apply(this,arguments)},u=globalThis&&globalThis.__makeTemplateObject||function(a,n){return Object.defineProperty?Object.defineProperty(a,"raw",{value:n}):a.raw=n,a},B=globalThis&&globalThis.__assign||function(){return B=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},B.apply(this,arguments)},On=l(F||(F=u([`
+ to {
+    stroke-dashoffset: 136;
+  }
+`],[`
+ to {
+    stroke-dashoffset: 136;
+  }
+`])));o.polygon(S||(S=u([`
+  stroke-dasharray: 17;
+  animation: `,` 2.5s cubic-bezier(0.35, 0.04, 0.63, 0.95) infinite;
+`],[`
+  stroke-dasharray: 17;
+  animation: `,` 2.5s cubic-bezier(0.35, 0.04, 0.63, 0.95) infinite;
+`])),On);o.svg(q||(q=u([`
+  transform-origin: 50% 65%;
+`],[`
+  transform-origin: 50% 65%;
+`])));var F,S,q,N=globalThis&&globalThis.__assign||function(){return N=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},N.apply(this,arguments)},G=globalThis&&globalThis.__assign||function(){return G=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},G.apply(this,arguments)},H=globalThis&&globalThis.__assign||function(){return H=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},H.apply(this,arguments)},J=globalThis&&globalThis.__assign||function(){return J=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},J.apply(this,arguments)},K=globalThis&&globalThis.__assign||function(){return K=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},K.apply(this,arguments)},M=globalThis&&globalThis.__assign||function(){return M=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},M.apply(this,arguments)},Q=globalThis&&globalThis.__assign||function(){return Q=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},Q.apply(this,arguments)},V=globalThis&&globalThis.__assign||function(){return V=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},V.apply(this,arguments)},X=globalThis&&globalThis.__assign||function(){return X=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},X.apply(this,arguments)},Y=globalThis&&globalThis.__assign||function(){return Y=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},Y.apply(this,arguments)},Z=globalThis&&globalThis.__assign||function(){return Z=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},Z.apply(this,arguments)},nn=globalThis&&globalThis.__assign||function(){return nn=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},nn.apply(this,arguments)},tn=globalThis&&globalThis.__assign||function(){return tn=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},tn.apply(this,arguments)},rn=globalThis&&globalThis.__assign||function(){return rn=Object.assign||function(a){for(var n,r=1,e=arguments.length;r<e;r++){n=arguments[r];for(var t in n)Object.prototype.hasOwnProperty.call(n,t)&&(a[t]=n[t])}return a},rn.apply(this,arguments)};export{kn as R};
---
+++ assets/styled-tools-bf41095e.js
@@ -0,0 +1,1 @@
+var d=function(r,i){return function(){var e=arguments.length>0&&arguments[0]!==void 0?arguments[0]:{};if(typeof e[r]<"u")return e[r];if(r&&r.indexOf(".")>0){for(var u=r.split("."),l=u.length,n=e[u[0]],f=1;n!=null&&f<l;)n=n[u[f]],f+=1;if(typeof n<"u")return n}return i}};export{d as p};
